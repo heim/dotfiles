badb049884cdf7d57e8e8858817b9c1e098d4b02
Andreas Heim <andreas@heim.no>
2015-07-01 23:51:08 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 #! /bin/sh
 
-ln -s ~/dotfiles/.colorizerc ~/.colorizerc
-ln -s ~/dotfiles/.dircolors ~/.dircolors
-ln -s ~/dotfiles/.gemrc ~/.gemrc
-ln -s ~/dotfiles/.gitconfig ~/.gitconfig
-ln -s ~/dotfiles/.irbrc ~/.irbrc
+ln -s ~/.dotfiles/.colorizerc ~/.colorizerc
+ln -s ~/.dotfiles/.dircolors ~/.dircolors
+ln -s ~/.dotfiles/.gemrc ~/.gemrc
+ln -s ~/.dotfiles/.gitconfig ~/.gitconfig
+ln -s ~/.dotfiles/.irbrc ~/.irbrc
