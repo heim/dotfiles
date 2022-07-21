16b1d4db86e201e934aba4fa63387b892de67bea
Andreas Heim <andreas@heim.no>
2022-07-21 09:57:39 +0200

    remove autocomplete plugin

--- oh-my-zsh-custom/environment_variables.zsh
+++ oh-my-zsh-custom/environment_variables.zsh
@@ -1,6 +1,6 @@
 export EDITOR='vi'
 #export GIT_EDITOR='vi'
-export PATH="$HOME/dotfiles/bin:$HOME/bin:$HOME/.bin:/usr/local/bin:/usr/local/sbin:/usr/local/mysql/bin:/usr/local/git/bin:$PATH"
+export PATH="$HOME/.local/bin/:$HOME/dotfiles/bin:$HOME/bin:$HOME/.bin:/usr/local/bin:/usr/local/sbin:/usr/local/mysql/bin:/usr/local/git/bin:$PATH"
 export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"
 export HOMEBREW_PREFIX=/usr/local
 export HOMEBREW_CELLAR=/usr/local/Cellar
