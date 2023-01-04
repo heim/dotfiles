a719598b5b5917bedd513c61deb618f1eaacf671
Andreas Heim <andreas@heim.no>
2023-01-04 15:14:38 +0100

    custom starship gcloud prompt

--- oh-my-zsh-custom/environment_variables.zsh
+++ oh-my-zsh-custom/environment_variables.zsh
@@ -1,6 +1,7 @@
-export EDITOR='vi'
+export EDITOR='lvim'
 #export GIT_EDITOR='vi'
 export PATH="$HOME/.local/bin/:$HOME/dotfiles/bin:$HOME/bin:$HOME/.bin:/usr/local/bin:/usr/local/sbin:/usr/local/mysql/bin:/usr/local/git/bin:$PATH"
 export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"
 export HOMEBREW_PREFIX=/usr/local
 export HOMEBREW_CELLAR=/usr/local/Cellar
+export STARSHIP_CONFIG="$HOME/dotfiles/starship.toml"
