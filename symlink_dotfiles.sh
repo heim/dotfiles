9cfe6e37737f64968a4f68670a7ff6605d443996
Andreas Heim <andreas@heim.no>
2023-02-09 21:46:53 +0100

    delete target files first

--- symlink_dotfiles.sh
+++ symlink_dotfiles.sh
@@ -1,6 +1,10 @@
 #! /bin/sh
 
+rm ~/.colorizerc
 ln -s ~/dotfiles/colorizerc ~/.colorizerc
+rm ~/.dircolors
 ln -s ~/dotfiles/dircolors ~/.dircolors
+rm ~/.gitconfig
 ln -s ~/dotfiles/gitconfig ~/.gitconfig
+rm ~/.tmux.conf
 ln -s ~/dotfiles/tmux.conf ~/.tmux.conf
