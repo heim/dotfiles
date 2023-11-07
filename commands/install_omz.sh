0d642c70bdfdb7a9c7a7d82d548a7cbb98473338
Andreas Heim <andreas@heim.no>
2023-11-07 10:31:34 +0100

    TIL: ZSH_CUSTOM 🤦🏻

--- commands/install_omz.sh
+++ commands/install_omz.sh
@@ -6,14 +6,10 @@ if ! [[ -d ~/.oh-my-zsh ]]; then
   sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 fi
 
-echo "Linking custom oh-my-zsh stuff"
-if [[ -f ~/.oh-my-zsh/custom/example.zsh ]]; then
-  echo "Example folder exists in oh-my-zsh. Deleting"
-  rm -r ~/.oh-my-zsh/custom
-  ln -s ~/dotfiles/oh-my-zsh-custom ~/.oh-my-zsh/custom
+#check if ~/.zshrc exists
+# if it does, remove it
+if [[ -f $HOME/.zshrc ]]; then
+  rm $HOME/.zshrc
 fi
 
-if [[ -f  ~/.zshrc ]]; then
-  rm ~/.zshrc
-fi
-ln -s ~/.oh-my-zsh/custom/zshrc ~/.zshrc
+ln -s $HOME/dotfiles/oh-my-zsh-custom/zshrc $HOME/.zshrc
