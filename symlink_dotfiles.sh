bce076be8743cfd0eeb1285bb49b0e6ca533757e
Andreas Heim <andreas@heim.no>
2023-11-29 12:28:52 +0100

    install kitty and monaspace font

--- symlink_dotfiles.sh
+++ symlink_dotfiles.sh
@@ -10,3 +10,5 @@ rm ~/.tmux.conf
 ln -s ~/dotfiles/tmux.conf ~/.tmux.conf
 rm -rf ~/.config/nvim
 ln -s ~/dotfiles/nvim ~/.config/nvim
+rm -rf ~/.config/kitty
+ln -s ~/dotfiles/kitty ~/.config/kitty
