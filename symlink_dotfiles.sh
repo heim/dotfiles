4968aedc22ff3b0e8286f87d7f648cb79b0186d1
Andreas Heim <andreas@heim.no>
2023-12-11 14:53:12 +0100

    updatez 🫓

--- symlink_dotfiles.sh
+++ symlink_dotfiles.sh
@@ -1,9 +1,5 @@
 #! /bin/sh
 
-rm ~/.colorizerc
-ln -s ~/dotfiles/colorizerc ~/.colorizerc
-rm ~/.dircolors
-ln -s ~/dotfiles/dircolors ~/.dircolors
 rm ~/.gitconfig
 ln -s ~/dotfiles/gitconfig ~/.gitconfig
 rm ~/.tmux.conf
